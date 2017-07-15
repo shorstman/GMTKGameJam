a913d0eef6978b969a8bb9251402250a5ce482c9
shorstman <horstman.s@husky.neu.edu>
2017-07-14 20:06:39 -0400

    Added comments and cleaned up Main.py

--- Obstacle.py
+++ Obstacle.py
@@ -1,8 +1,8 @@
 import pygame
 
 class Obstacle():
-    def __init__(self, x, y):
-        self.rect = pygame.Rect(x, y, 50, 50)
+    def __init__(self, x, y, width, height):
+        self.rect = pygame.Rect(x, y, width, height)
 
     def drawObstacle(self, window):
         pygame.draw.rect(window, (168, 168, 168), self.rect)
