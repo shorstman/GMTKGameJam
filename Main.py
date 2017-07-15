7973de78e5047b1db958a2cca06de1d0c2e6e777
shorstman <horstman.s@husky.neu.edu>
2017-07-14 20:07:20 -0400

    Added comments and cleaned up Main.py

--- Main.py
+++ Main.py
@@ -6,29 +6,33 @@ from pygame.locals import *
 NAME = "GMTKGameJam"
 
 def main():
+    #Initialize the window
     pygame.init()
-    window = pygame.display.set_mode((900, 600))
+    window = pygame.display.set_mode((928, 600))
     pygame.display.set_caption(NAME)
 
+    #Create the background object
     background = pygame.Surface(window.get_size())
     background = background.convert()
     background.fill((250, 250, 250))
 
+    #Draw the background
     window.blit(background, (0,0))
     pygame.display.flip()
 
+    #Create the player object
     player = Player(0, 0)
-    obstacle = Obstacle(450, 300)
+    obstacles = []
+    for x in range(0, 29):
+        obstacles.append(Obstacle(x*50, 250, 32, 32))
 
-    x = 0
-    y = 0
-    moveX = 2
-    moveY = 2
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
                 return
+
         keys = pygame.key.get_pressed()
+        #Movement
         if keys[pygame.K_UP]:
             player.moveY(-3)
         if keys[pygame.K_DOWN]:
@@ -38,8 +42,14 @@ def main():
             player.moveX(-3)
         if keys[pygame.K_RIGHT]:
             player.moveX(3)
+
+        #Check collisions
         player.checkCollisions(obstacle)
+
+        #Refresh the background
         window.blit(background, (0,0))
+
+        #Draw the player
         player.drawPlayer(window)
         obstacle.drawObstacle(window)
         pygame.display.update()
